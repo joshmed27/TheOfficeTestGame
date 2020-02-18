f151105fb7767e853a03f3cfa58f84a46fd227bb
Joshua Medina <strog21000@gmail.com>
2020-02-17 22:40:15 -0800

    Script Changes
    Added player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    // Movement Speed of Player
+    public float mvtSpeed;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //If forward movement key "w" is pressed then Vector3 is adjusted in the upwards direction
+        if(Input.GetKey("w"))
+        {
+            transform.position += transform.TransformDirection(Vector3.up) * Time.deltaTime * mvtSpeed;
+        }
+        //If forward movement key "s" is pressed then Vector3 is adjusted in the upwards direction
+        else if (Input.GetKey("s"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.up) * Time.deltaTime * mvtSpeed;
+        }
+        //If forward movement key "a" and not "d" is pressed then Vector3 is adjusted in the upwards direction
+        if (Input.GetKey("a") && !Input.GetKey("d"))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * mvtSpeed;
+        } else if (!Input.GetKey("a") && Input.GetKey("d"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * mvtSpeed;
+        }
+    }
+}
