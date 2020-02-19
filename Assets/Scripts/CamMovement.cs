4821d0c9f16ddff1af7942c7f6f22e2159d0a093
Joshua Medina <strog21000@gmail.com>
2020-02-18 22:27:15 -0800

    Ref Map and Camera Movment Adjusted
    Camera is now properly attached to character

---
+++ Assets/Scripts/CamMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    // Movement Speed of Player
+    public float mvtSpeed;
+    private float camMomentum;
+
+    // FixedUpdate is called once per frame
+    void FixedUpdate()
+        
+    {
+        //If forward movement key "w" is pressed then Vector2 is adjusted in the upwards direction else if "s" is pressed it is adjusted in the negative
+        if (Input.GetKey("w") && !Input.GetKey("s"))
+        {
+            transform.position += transform.TransformDirection(Vector2.up) * Time.deltaTime * mvtSpeed;
+        }
+        else if (!Input.GetKey("w") && Input.GetKey("s"))
+        {
+            transform.position -= transform.TransformDirection(Vector2.up) * Time.deltaTime * mvtSpeed;
+        }
+
+        //If forward movement key "a" is pressed then Vector2 is adjusted in the left direction else if "d" is pressed it is adjusted in the negative
+        if (Input.GetKey("a") && !Input.GetKey("d"))
+        {
+            transform.position += transform.TransformDirection(Vector2.left) * Time.deltaTime * mvtSpeed;
+        }
+        else if (!Input.GetKey("a") && Input.GetKey("d"))
+        {
+            transform.position -= transform.TransformDirection(Vector2.left) * Time.deltaTime * mvtSpeed;
+        }
+    }
+}
