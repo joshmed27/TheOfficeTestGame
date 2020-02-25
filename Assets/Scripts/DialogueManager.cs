0c776d4f11f345dc4936d5a4077820ce3bdecb00
Lilmergo <megan.nguyenn261@gmail.com>
2020-02-24 23:20:45 -0800

    Added some art assets and started on dialogue system
    Art assets added:
    - 1 vertical desk
    - 1 receptionist desk
    - 1 talk button (to be implemented once dialogue system is complete)
    
    Scripts added:
    - Dialogue manager
    - Dialogue
    * check comments in scripts, followed tutorial: https://www.youtube.com/watch?v=_nRzoTzeyxU

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences; //keep track of all current sentences in dialogue
+    void Start()
+    {
+        sentences = new Queue<string>();
+
+    }
+
+} 
