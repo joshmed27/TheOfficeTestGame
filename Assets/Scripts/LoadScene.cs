5edb8ad808cb552b91051ec73d82ca02aae52e36
Joshua Medina <joshuamed27@gmail.com>
2020-03-14 21:50:58 -0700

    Added Scripts & Scene Tweaks
    - Added Scripts for future editing(MichealMovement)
    - Added Exit and Play Button Scripts
    - Added Chair Templates

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void LoadSCNE(string SceneName)
+    {
+        //Using Scene Manager to load the selected scene
+        SceneManager.LoadScene(SceneName);
+    }
+}
